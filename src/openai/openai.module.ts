5054ca3bed485c8fe15daa824f3b2b09dc263399
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-08-14 20:34:34 +0300

    feat: setup OpenaiModule with OpenAI client

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
 import { OpenaiController } from './openai.controller';
+import OpenAI from 'openai';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [OpenaiService],
-  controllers: [OpenaiController]
+  providers: [
+    OpenaiService,
+    {
+      provide: OpenAI,
+      useFactory: (configService: ConfigService) =>
+        new OpenAI({ apiKey: configService.getOrThrow('OPENAI_API_KEY') }),
+      inject: [ConfigService],
+    },
+  ],
+  controllers: [OpenaiController],
+  imports: [ConfigModule],
 })
 export class OpenaiModule {}
