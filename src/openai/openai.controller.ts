21baa8c1c57ab62afa35b4bff3998b87996abb88
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-08-14 21:07:31 +0300

    feat: add POST /openai/chatCompletion endpoint

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateChatCompletionRequest } from './dtos/create-chat-compleiton.request';
+import { OpenaiService } from './openai.service';
 
 @Controller('openai')
 export class OpenaiController {
+  constructor(private readonly openaiService: OpenaiService) {}
   @Post('chatCompletion')
   async createChatCompletion(@Body() body: CreateChatCompletionRequest) {
-    
+    return this.openaiService.createChatCompletion(body.messages);
   }
 }
